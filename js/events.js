3b2b6972eecdd065985539b1a3effeecab2f06c9
Matteo Muscas <thatman.j@gmail.com>
2018-09-24 11:56:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,23 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+}
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  });
+}
+function pressIt() {
+  $('form').on('keydown', function(key){
+  if (key.which == 71)
+    alert('Pesciolini');
+  });
+}
+
+
 $(document).ready(function(){
 
 // call functions here
